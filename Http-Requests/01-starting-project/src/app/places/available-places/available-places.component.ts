7a232b6d8721a980c18373fd830c8f4372e0aca6
JahnaviPeraka <Jahnav.Peraka@CignaHealthcare.com>
2025-02-05 02:11:27 -0500

    Http-Request

---
+++ Http-Requests/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,64 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+
+  // constructor(private httpClietn: HttpClient){}
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadAvailablePlaces().subscribe({
+      // next: (response) => {
+      //   console.log(response.body?.places);
+      // }
+      // next: (event) => {
+      //   console.log(event);
+      // }
+      // next: (resData) => {
+      //   console.log(resData.places);
+      // }
+      next: (places) => {
+        this.places.set(places);
+      },
+      error: (error: Error) => {
+        // console.log(error);
+        this.error.set(error.message);
+        // this.error.set('Something went wrong fetching the available places. Please try again later.');
+      },
+      complete: () => {
+        this.isFetching.set(false)
+      } 
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  onSelectPlace(selectedPlace: Place){
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace).subscribe({
+      next: (resData) => console.log(resData)
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+}
